a2c2e66247da712dbc65d0c2102d6ef1b152ed4b
Julian <julianfrank125@gmail.com>
2024-12-05 12:12:50 -0700

    Final cleaning and commenting

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 
 int main()
 {
+    // Initialize instance of TrafficLight
     TrafficLight traffic_light;
     initialize_traffic_light(&traffic_light, GREEN_DURATION, YELLOW_DURATION, RED_DURATION);
 
+    // Begin traffic light simulation
     printf("Simulating Traffic Light\n");
     simulate_traffic_light(&traffic_light);
     return 0;
